bad5c1e53e4307f237a4fbf4a9e349e2df0c4106
alexa-sl <>
2023-08-16 10:33:44 +0300

    added unversioned

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import {ServicesPageComponent} from './services-page/services.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { ContactsPageComponent } from './contacts-page/contacts-page.component';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import {NgOptimizedImage} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    MainLayoutComponent,
+    ServicesPageComponent,
+    FaqPageComponent,
+    ContactsPageComponent,
+    AboutUsPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgOptimizedImage
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
