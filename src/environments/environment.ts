7a676061ec9d629d2355a9fab1731a8ba52d799c
alexa-sl <>
2023-09-05 19:31:07 +0300

    back path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  authApiUrl: 'https://rusprominwest.ru',
-  ordersApiUrl: 'https://rusprominwest.ru'
+  authApiUrl: '185.84.126.23:7000',
+  ordersApiUrl: '185.84.126.23:7000'
 };
