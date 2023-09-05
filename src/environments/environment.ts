d9886afde085bd18a898c66828966fcbf1a1f1e9
alexa-sl <>
2023-09-05 15:17:20 +0300

    env api path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  authApiUrl: 'http://localhost:5000',
-  ordersApiUrl: 'http://localhost:5000'
+  authApiUrl: 'https://rusprominwest.ru',
+  ordersApiUrl: 'https://rusprominwest.ru'
 };
