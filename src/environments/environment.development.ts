d6f2d392b6c13fbf5a220df0bad525bfcf84b698
alexa-sl <>
2023-10-18 14:55:14 +0300

    API test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,7 @@
 export const environment = {
-  // authApiUrl: 'http://localhost:5000',
-  // ordersApiUrl: 'http://localhost:5000'
+  // authApiUrl: 'http://localhost:8080',
+  // ordersApiUrl: 'http://localhost:8080'
 
-  //authApiUrl: '185.84.126.23:7000',
-  //ordersApiUrl: '185.84.126.23:7000'
-
-  authApiUrl: 'https://rusprominwest.ru',
-  ordersApiUrl: 'https://rusprominwest.ru'
+  authApiUrl: 'https://185.84.162.23',
+  ordersApiUrl: 'https://185.84.162.23'
 };
