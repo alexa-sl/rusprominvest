052b89782a1f7d3f5418228a2b1639e50acf5316
alexa-sl <>
2023-10-21 13:43:18 +0300

    DomainInterceptor (replace API path for rf domain calls)

--- src/app/helpers/domain.interceptor.ts
+++ src/app/helpers/domain.interceptor.ts
@@ -16,24 +16,20 @@ export class DomainInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const domain = this.document.location.hostname;
-    const rfDomainApi = 'https://xn--b1aghpgdghbiimc.xn--p1ai/';
     const rfDomain = 'xn--b1aghpgdghbiimc.xn--p1ai';
+    const rfDomainApi = 'https://xn--b1aghpgdghbiimc.xn--p1ai/';
     const getDomain = function(string: string, subString: string, index: number) {
       return string.split(subString, index).join(subString);
     };
-    const originalDomain = getDomain(req.url, '/', 3);
+    const reqDomain = getDomain(req.url, '/', 3);
 
-    const replacedUrl = req.url.replace(originalDomain, rfDomainApi);
+    const replacedUrl = req.url.replace(reqDomain, rfDomainApi);
     let updReq = req;
 
     if (domain === rfDomain) {
       updReq = req.clone({url: replacedUrl});
     }
 
-    console.log('domain', domain);
-    console.log('originalDomain', originalDomain);
-    console.log('replacedUrl', replacedUrl);
-    console.log('updReq', updReq);
     return next.handle(updReq);
   }
 }
