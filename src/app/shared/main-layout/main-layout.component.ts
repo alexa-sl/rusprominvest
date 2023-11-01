d9d49de9defbf7359466b4e8b6f938a51bd2fd8f
alexa-sl <>
2023-11-01 13:35:41 +0300

    Close navigation after route change

--- src/app/shared/main-layout/main-layout.component.ts
+++ src/app/shared/main-layout/main-layout.component.ts
@@ -1,15 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {combineLatest} from "rxjs";
+import {combineLatest, filter} from "rxjs";
 import {Store} from "@ngrx/store";
 import {selectCurrentUser} from "../../auth/store/reducers/reducers";
+import {NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.sass']
 })
-export class MainLayoutComponent implements OnInit{
- constructor(private store: Store) {}
+export class MainLayoutComponent implements OnInit {
+ constructor(private store: Store, private router: Router) {}
   active = false;
   data$ = combineLatest({
     currentUser: this.store.select(selectCurrentUser)
@@ -17,8 +18,14 @@ export class MainLayoutComponent implements OnInit{
 
   ngOnInit() {
     this.active = false;
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe(() => {
+      this.active = false;
+    })
   };
   onBurgerClicked() {
     this.active = !this.active;
   }
+
 }
